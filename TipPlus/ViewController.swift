423b04b8af8d4dcb08097e215271080f9d85d3d8
William Ordaz <william_ordaz@yahoo.com>
2020-01-20 19:33:50 -0800

    Initial Commit

---
+++ TipPlus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipPlus
+//
+//  Created by William Ordaz on 1/20/20.
+//  Copyright © 2020 William Ordaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
