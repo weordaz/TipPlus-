9b4dfaf726e7af4bf0a20089f62566717833cbb9
William Ordaz <william_ordaz@yahoo.com>
2020-01-21 21:40:51 -0800

    My Changes

--- TipPlus/ViewController.swift
+++ TipPlus/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentages = [0.09, 0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        let splitPeople = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+        
+        let split = total / splitPeople[splitControl.selectedSegmentIndex]
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        splitLabel.text = String(format: "$%.2f", split)
+    }
 }
 
